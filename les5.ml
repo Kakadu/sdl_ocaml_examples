47c66092a8fb4a1974fd1460a63f3c35d2d6588a
kakadu <kakadu.hafanana@gmail.com>
2013-12-21 18:50:04 +0400

    Lesson 5 finished

--- les5.ml
+++ les5.ml
@@ -21,7 +21,8 @@ let render_texture_in ?clip ~texture ~renderer ~x ~y =
   )
 
 let main () =
-  create_window "Lesson 5" ~x:100 ~y:100 ~w:screen_width ~h:screen_height Window.opengl >>= fun window ->
+  create_window "Lesson 5 (try keys 1,2,3,4)"
+    ~x:100 ~y:100 ~w:screen_width ~h:screen_height Window.opengl >>= fun window ->
   create_renderer ~flags:Renderer.(accelerated + presentvsync) window >>= fun renderer ->
   img_load_texture renderer "res/lesson5/image.png" >>= fun image ->
   query_texture image >>= fun (_,_,(iW,iH)) ->
@@ -35,17 +36,28 @@ let main () =
     ]
   in
   let e = Event.create () in
+  let n = ref 1 in
   let rec loop last =
     match last with
     | `Error -> `Error
     | `Ok _ -> begin
         let quit = ref false in
         while poll_event (Some e) do
-          let x = Event.get e Event.typ in
-          if x=Event.quit || x = Event.mouse_button_down || x = Event.key_down then quit := true
+          let etyp = Event.get e Event.typ in
+          if etyp = Event.quit ||
+             etyp = Event.mouse_button_down then quit := true
+          else
+            let keycode = Event.get e Event.keyboard_keycode in
+            if keycode = K.escape then quit := true else
+            if keycode = K.k1 then n:=1 else
+            if keycode = K.k2 then n:=2 else
+            if keycode = K.k3 then n:=3 else
+            if keycode = K.k4 then n:=4
+
         done;
         let init = render_clear renderer in
-        List.fold_left (fun acc (clip,(x,y)) -> acc >>= fun _ ->
+        List.fold_left (fun acc (_,(x,y)) -> acc >>= fun _ ->
+          let clip = List.nth clips (!n-1) |> fst in
           render_texture_in ~clip ~texture:image ~renderer ~x ~y) init clips
         >>= fun _ ->
         render_present renderer;
